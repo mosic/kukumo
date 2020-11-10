41bc02cddf5efe38c136d513efd0f61d462959f4
Miloš Mošić <mosic.milos@gmail.com>
2020-11-10 10:27:48 +0000

    Remove support for Elixir versions lower than 1.7

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -13,7 +13,8 @@ defmodule CabbageTestHelper do
   def run(filters \\ [], cases \\ [])
 
   def run(filters, module) do
-    {add_module, load_module, result_fix} = versioned_callbacks()
+    add_module = &ExUnit.Server.add_async_module/1
+    load_module = &ExUnit.Server.modules_loaded/0
 
     Enum.each(module, add_module)
     load_module.()
@@ -24,22 +25,6 @@ defmodule CabbageTestHelper do
       |> Keyword.merge(colors: [enabled: false])
 
     output = capture_io(fn -> Process.put(:capture_result, ExUnit.Runner.run(opts, nil)) end)
-    {result_fix.(Process.get(:capture_result)), output}
-  end
-
-  defp versioned_callbacks() do
-    System.version()
-    |> Version.compare("1.6.6")
-    |> case do
-      :lt -> {&ExUnit.Server.add_sync_case/1, &ExUnit.Server.cases_loaded/0, &fix_13_elixir_test_result/1}
-      :eq -> {&ExUnit.Server.add_async_module/1, &ExUnit.Server.modules_loaded/0, &fix_13_elixir_test_result/1}
-      _ -> {&ExUnit.Server.add_sync_module/1, &ExUnit.Server.modules_loaded/0, &fix_17_elixir_test_result/1}
-    end
-  end
-
-  defp fix_17_elixir_test_result(result), do: result
-
-  defp fix_13_elixir_test_result(result) do
-    Map.merge(result, %{excluded: Map.get(result, :skipped, 0), skipped: 0})
+    {Process.get(:capture_result), output}
   end
 end
